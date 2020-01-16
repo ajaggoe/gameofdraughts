3cc5792be0ed15ea6b768544c63c016aae744cd1
ThomBreugelmans <59830003+ThomBreugelmans@users.noreply.github.com>
2020-01-16 16:48:18 +0100

    Add files via upload

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -8,7 +8,8 @@ window.onload = function() {
                         [-1    ,8    ,-1    ,7      ,-1     ,6      ,-1     ,5 ],
                         [4     ,-1   ,3     ,-1     ,2      ,-1     ,1      ,-1]];
 
-
+    //used to save all piece instances
+    var pieces = [];
 
 
     var Board = {
@@ -21,15 +22,59 @@ window.onload = function() {
         init: function (){
             for(row in this.board) {
                 for(column in this.board[row]) {
-                    if(board[row][column] !== -1) {
-                        let tile = document.createElement('div');
-                        tile.style.width = '64px';
-                        tile.style.height = '64px';
-                        tile.style.backgroundColor = 'black';
-                        tile.style.top = this.location[row];
-                        tile.style.left = this.location[column];
-                        this.tiles.append(tile);  
+                    if(row%2==0) {
+                        if(column%2==1) {
+                            let tile = document.createElement('div');
+                            tile.style.width = '64px';
+                            tile.style.height = '64px';
+                            tile.style.backgroundColor = 'black';
+                            tile.style.position = 'absolute';
+                            tile.style.top = this.location[row];
+                            tile.style.left = this.location[column];
+                            this.tiles.append(tile); 
+                        } 
                         
+                    }else {
+                        if(column%2==0) {
+                            let tile = document.createElement('div');
+                            tile.style.width = '64px';
+                            tile.style.height = '64px';
+                            tile.style.backgroundColor = 'black';
+                            tile.style.position = 'absolute';
+                            tile.style.top = this.location[row];
+                            tile.style.left = this.location[column];
+                            this.tiles.append(tile); 
+                        }
+                    }
+                    if(this.board[row][column] > 12) {
+                        let piece = document.createElement('div');
+                        piece.id = this.board[row][column].toString();
+                        piece.style.width = '60px';
+                        piece.style.height = '60px';
+                        piece.style.margin = '2px';
+                        piece.style.borderRadius = '50%';
+                        piece.style.backgroundColor = 'red';
+                        piece.style.position = 'absolute';
+                        piece.style.top = this.location[row];
+                        piece.style.left = this.location[column];
+                        $('.redpiece').append(piece);
+
+                        pieces[this.board[row][column]] = new Piece($("#"+this.board[row][column]),[parseInt(row),parseInt(column)]);
+                    }
+                    else if(this.board[row][column] < 13 && this.board[row][column] > 0) {
+                        let piece = document.createElement('div');
+                        piece.id = this.board[row][column].toString();
+                        piece.style.width = '60px';
+                        piece.style.height = '60px';
+                        piece.style.margin = '2px';
+                        piece.style.borderRadius = '50%';
+                        piece.style.backgroundColor = 'white';
+                        piece.style.position = 'absolute';
+                        piece.style.top = this.location[row];
+                        piece.style.left = this.location[column];
+                        $('.whitepiece').append(piece);
+
+                        pieces[this.board[row][column]] = new Piece($("#"+this.board[row][column]),[parseInt(row),parseInt(column)]);
                     }
                 }
             }
@@ -41,11 +86,34 @@ window.onload = function() {
 
     // position = [x,y]
     function Tile(element, position) {
-        //element on the doc
+        //element on the DOM
         this.element = element;
         //position on the board [x,y]
         this.position = position;
     }
+
+    // position = [x,y]
+    function Piece(element, position) {
+        //element on the DOM
+        this.element = element;
+        //position on the board [x,y]
+        this.position = position;
+        this.player = '';
+        // assigns a piece to the corresponding player by checking the id
+        if(this.element.attr('id') < 13) {
+            this.player = 1;
+        } else {
+            this.player = 2;
+        }
+        this.isKing = false;
+        //TODO
+        // this.makeKing = function () {};
+
+        // all pieces are allowed to move initially
+        this.canMove = true;
+    }
+
+    Board.init();
 }
 /*
 function Tile{
